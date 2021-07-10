e8ddcc7935386e4f3244e1de2d533044abf4243e
divineLush <alvipath@yahoo.com>
2021-07-10 10:04:55 -0400

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>123</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App"
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  font-family: monospace;
+  font-size: 1.6rem;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 4rem;
 }
 </style>
